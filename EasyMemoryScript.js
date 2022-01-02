c54f0175909f76207309e672d04ff386d9c60a39
DontAskForAnything00 <misiujasina@gmail.com>
2022-01-02 17:58:04 +0100

    AutoDivGeneration and ScriptDescription

--- EasyMemoryScript.js
+++ EasyMemoryScript.js
@@ -1,6 +1,8 @@
+// Start when the initial HTML documnet has been completely loaded and parsed
 document.addEventListener('DOMContentLoaded', () => {
 
-const colors = [
+// Our array with cards
+const CardArray = [
     { color: 'red'},
     { color: 'blue'},
     { color: 'pink'},
@@ -10,6 +12,7 @@ const colors = [
     { color: 'salmon'},
     { color: 'sandybrown'},
 
+    // Pairs for cards above
     { color: 'red'},
     { color: 'blue'},
     { color: 'pink'},
@@ -20,23 +23,51 @@ const colors = [
     { color: 'sandybrown'}
 ]
 
- colors.sort(() => 0.5 - Math.random());
+// Randomizes the board if it didn't, the order of the cards would be as above, so ordered, and we don't want this
+ CardArray.sort(() => 0.5 - Math.random());
 
+// Create some arrays that'll store some temp variables
 let ChosenCards = [];
 let ChosenCardsId = [];
-var points = 0;
-const arrayofCards = Array.from(document.getElementsByClassName('card'));
 
+// Create a variable that'll store our points
+var points = 0;
+
+// Create array that will store all created divs with class 'card'
+var arrayofCards; 
+
+function createDivs(){
+
+    // This for loop creates div that are our "memory cards". Amount of card depends on our CardArray length
+    for(let i = 0; i < CardArray.length; i++){
+        var element = document.createElement("div");
+        element.classList.add("card");
+        document.querySelector('.game-container').appendChild(element);
+    }
+    
+    // Adds variables to arrayofCards created in line 37
+    arrayofCards = Array.from(document.getElementsByClassName('card'));
+
+}
+
+
+// Function that add data-id to every div that is in arrayofCards. Data-id will be useful for  defining with color card should have
 function createBorad(){
-    for(let i = 0; i < colors.length; i++){
+    createDivs();
+    for(let i = 0; i < CardArray.length; i++){
         arrayofCards[i].setAttribute('data-id' , i);
         arrayofCards[i].addEventListener('click', flipCard);
     }
 }
+
+// Function that check if our points are equal to amount of pairs
 function checkPoints(){
     points == arrayofCards.length/2 ? alert("You found all pairs! GG") : 1;
-    return;
 }
+
+// Function that checks Matches 
+//if found match add style vibility to thoes cards and add points
+// if not delete style 
 function CheckMatches(){
     if(ChosenCards[0] == ChosenCards[1]){
         alert('You found a match');
@@ -48,23 +79,26 @@ function CheckMatches(){
         alert('Try again');
         arrayofCards[ChosenCardsId[0]].addEventListener('click', flipCard);
         arrayofCards[ChosenCardsId[1]].addEventListener('click', flipCard);
-        arrayofCards[ChosenCardsId[0]].style.backgroundColor = '#eee';
-        arrayofCards[ChosenCardsId[1]].style.backgroundColor = '#eee';
-
+        arrayofCards[ChosenCardsId[0]].style = '';
+        arrayofCards[ChosenCardsId[1]].style = '';
     }
-
+    
+    // also clear our temp variables 
     ChosenCardsId = [];
     ChosenCards = [];
 }
+
+// Function that "Flip Card" so basically show our hidden color
 function flipCard(){
     let cardId = this.getAttribute('data-id');
-    this.style.backgroundColor = colors[cardId].color;
-    ChosenCards.push(colors[cardId].color);
+    this.style.backgroundColor = CardArray[cardId].color;
+    ChosenCards.push(CardArray[cardId].color);
     ChosenCardsId.push(cardId);
     this.removeEventListener('click', flipCard);
     ChosenCards.length == 2 ? setTimeout(CheckMatches, 50) : 1;
 }
 
+// Executes "createBorad" function
 createBorad();
 });
 
