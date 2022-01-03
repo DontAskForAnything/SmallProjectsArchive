afb7066912c3f6c6b0460ac96d265df449ba7cd7
DontAskForAnything00 <misiujasina@gmail.com>
2022-01-03 11:09:35 +0100

    Description added

--- EasyRandomCatImageScript.js
+++ EasyRandomCatImageScript.js
@@ -1,8 +1,17 @@
+// Variable declaration
 const button = document.querySelector('button');
 const meal_constainer = document.querySelector("#meal");
+
+// Adding event listener on click to button
 button.addEventListener('click', () => {
+
+    // When button clicked, it fetches data from api
     fetch('https://aws.random.cat/meow')
+
+    // After fetching string data changes to json format
     .then(res => res.json())
+
+     // After format changes, changes img attribute display and sets img src as a link from api
     .then(res => { 
         document.querySelector('img').style.display = "";
         document.querySelector('img').src = res.file
